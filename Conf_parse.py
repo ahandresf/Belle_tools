fc5bb4bd5e8ca3df002c36ecee57aa5c2d83fcb5
ahandresf <36319841ahandresf@users.noreply.github.com>
2018-07-05 14:09:19 -0400

    new direction, writing in another file

--- Conf_parse.py
+++ Conf_parse.py
@@ -3,10 +3,9 @@ import json
 
 input_filename='TestFile.cfg'
 #input_filename='BelleCertification.cfg'
-intermedian_filename='BelleIntermedian.json'
-output_filename="BelleCertification.json"
+output_filename='BelleCertification.json'
 f_in=open(input_filename,'r')
-f_out=open(intermedian_filename,'w+')
+f_out=open(output_filename,'w+')
 #f_in.next()
 
 def line_parser(l_in):
@@ -39,7 +38,7 @@ def line_parser(l_in):
     #print(l_out)
     return(l_out)
 
-def ListTok(f_inter):
+def ListTok(f_final):
     state="no_token"
     previous_position=0
     flag=True
@@ -49,38 +48,38 @@ def ListTok(f_inter):
         if counter==42:
             flag=False
             print("going out of the script")
-        current_position=f_inter.tell() #It point to the bytes of this position.
-        line=f_inter.readline()
+        current_position=f_final.tell() #It point to the bytes of this position.
+        line=f_final.readline()
         print("\ncounter = ",counter)
         print("current_position",current_position)
         print("previous_position",previous_position)
-        print("pointer_tell",f_inter.tell())
+        print("pointer_tell",f_final.tell())
         print("state = ",state)
 
-        #for line in f_inter:
+        #for line in f_final:
         line=line.rstrip()
         #print(line)
         #print("current_position",current_position)
         if(state=="no_token"):
             if line.find("LIST_TOKEN_DECORATOR")!=-1:
                 print("I found a token, no_token_before")
-                #print("my current position is",f_inter.tell())
+                #print("my current position is",f_final.tell())
                 l_out_current="'"+line.split(",")[1]+"',\n"
                 print("l_out_current", l_out_current)
-                f_inter.write(l_out_current) #modifying the current line
+                f_final.write(l_out_current) #modifying the current line
                 print("previous_position",previous_position)
-                f_inter.seek(previous_position)
-                l_out_before=f_inter.readline().rstrip().split(":",1)
+                f_final.seek(previous_position)
+                l_out_before=f_final.readline().rstrip().split(":",1)
                 #l_out_before=l_out_before.rstrip()
                 #l_out_before=l_out_before.split(":")
                 l_out_before=l_out_before[0]+": ["+l_out_before[1]+",\n"
                 print("l_out_before", l_out_before)
-                print("pointer before write",f_inter.tell())
-                f_inter.write(l_out_before) #modifying the previous line
-                print("pointer after write",f_inter.tell())
+                print("pointer before write",f_final.tell())
+                f_final.write(l_out_before) #modifying the previous line
+                print("pointer after write",f_final.tell())
 
-                f_inter.seek(current_position) #putting back the pointer in the current position.
-                print("pointer after seek",f_inter.tell())
+                f_final.seek(current_position) #putting back the pointer in the current position.
+                print("pointer after seek",f_final.tell())
                 state="token" #updating State
             else:
                 print("pass_line")
@@ -89,13 +88,13 @@ def ListTok(f_inter):
             print("find_token with state token")
             if line.find("LIST_TOKEN_DECORATOR")!=-1:
                 l_out_current="'"+line.split(",")[1]+"',\n"
-                f_inter.write(l_out_current) #modifying the current line
+                f_final.write(l_out_current) #modifying the current line
             else:
-                f_inter.seek(previous_position)
-                l_out_before=f_inter.readline().strip(",\n")+"]"+"\n"
+                f_final.seek(previous_position)
+                l_out_before=f_final.readline().strip(",\n")+"]"+"\n"
                 print(l_out_before)
                 state="no_token"
-                f_inter.seek(current_position)
+                f_final.seek(current_position)
 
         #elif(state=="closing_token"):
         #    if line.find("LIST_TOKEN_DECORATOR")!=-1:
@@ -113,6 +112,6 @@ for line in f_in:
 f_in.close()
 f_out.close()
 
-f_inter=open(intermedian_filename,'r')
-ListTok(f_inter)
-f_inter.close()
+f_final=open(output_filename,'r+')
+ListTok(f_final)
+f_final.close()
